6f901c82767a60f7f7186b3f76356b826545f6dc
yuntianOS <365019475@qq.com>
2025-04-11 18:55:29 +0800

    tiptap 用在DocJsonEditor上

--- packages/core/client/src/schema-component/antd/doc-json-editor/DocJsonEditor.tsx
+++ packages/core/client/src/schema-component/antd/doc-json-editor/DocJsonEditor.tsx
@@ -9,49 +9,89 @@
 
 import { connect, mapProps, mapReadPretty } from '@formily/react';
 import React from 'react';
-import { lazy } from '../../../lazy-helper';
-import { isVariable } from '../../../variables/utils/isVariable';
 import { Input, ReadPretty as InputReadPretty } from '../input';
 import { useStyles } from './style';
 
-const ReactQuill = lazy(() => import('react-quill'));
+import { useField } from '@formily/react';
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import { Button, Space } from 'antd';
+import {
+  BoldOutlined,
+  ItalicOutlined,
+  StrikethroughOutlined,
+  OrderedListOutlined,
+  UnorderedListOutlined,
+  CodeOutlined,
+  UndoOutlined,
+  RedoOutlined,
+} from '@ant-design/icons';
 
 export const DocJsonEditor = connect(
   (props) => {
-    const { wrapSSR, hashId, componentCls } = useStyles();
+    const field = useField();
+    const { editor } = useStyles();
     const modules = {
       toolbar: [['bold', 'italic', 'underline', 'link'], [{ list: 'ordered' }, { list: 'bullet' }], ['clean']],
     };
-    const formats = [
-      'header',
-      'bold',
-      'italic',
-      'underline',
-      'strike',
-      'blockquote',
-      'list',
-      'bullet',
-      'indent',
-      'link',
-      'image',
-    ];
-    const { value, defaultValue, onChange, disabled } = props;
-    const resultValue = isVariable(value || defaultValue) ? undefined : value || '';
-    return wrapSSR(
-      <ReactQuill
-        className={`${componentCls} ${hashId}`}
-        modules={modules}
-        formats={formats}
-        value={resultValue}
-        onChange={(value) => {
-          if (value === '<p><br></p>') {
-            onChange('');
-          } else {
-            onChange(value);
-          }
-        }}
-        readOnly={disabled}
-      />,
+    const editorInstance = useEditor({
+      extensions: [StarterKit],
+      content: field.value || '',
+      onUpdate: ({ editor }) => {
+        field.onInput(editor.getJSON());
+      },
+    });
+
+    if (!editorInstance) {
+      return null;
+    }
+
+    return (
+      <div className={editor}>
+        <Space className="toolbar">
+          <Button
+            type={editorInstance.isActive('bold') ? 'primary' : 'default'}
+            icon={<BoldOutlined />}
+            onClick={() => editorInstance.chain().focus().toggleBold().run()}
+          />
+          <Button
+            type={editorInstance.isActive('italic') ? 'primary' : 'default'}
+            icon={<ItalicOutlined />}
+            onClick={() => editorInstance.chain().focus().toggleItalic().run()}
+          />
+          <Button
+            type={editorInstance.isActive('strike') ? 'primary' : 'default'}
+            icon={<StrikethroughOutlined />}
+            onClick={() => editorInstance.chain().focus().toggleStrike().run()}
+          />
+          <Button
+            type={editorInstance.isActive('bulletList') ? 'primary' : 'default'}
+            icon={<UnorderedListOutlined />}
+            onClick={() => editorInstance.chain().focus().toggleBulletList().run()}
+          />
+          <Button
+            type={editorInstance.isActive('orderedList') ? 'primary' : 'default'}
+            icon={<OrderedListOutlined />}
+            onClick={() => editorInstance.chain().focus().toggleOrderedList().run()}
+          />
+          <Button
+            type={editorInstance.isActive('code') ? 'primary' : 'default'}
+            icon={<CodeOutlined />}
+            onClick={() => editorInstance.chain().focus().toggleCode().run()}
+          />
+          <Button
+            icon={<UndoOutlined />}
+            onClick={() => editorInstance.chain().focus().undo().run()}
+            disabled={!editorInstance.can().undo()}
+          />
+          <Button
+            icon={<RedoOutlined />}
+            onClick={() => editorInstance.chain().focus().redo().run()}
+            disabled={!editorInstance.can().redo()}
+          />
+        </Space>
+        <EditorContent editor={editorInstance} />
+      </div>
     );
   },
   mapProps({
